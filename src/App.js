5bdc872fbe22eb2d0661c22422280bf3f479b9ba
Kevin-Estrada <ke81297@gmail.com>
2021-11-22 23:28:32 -0600

    This is just pushing all existing code to deploy on Heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import EmployeesList from './components/EmployeesList';
+import AddEmployee from './components/AddEmployee';
+import NotFound from './components/NotFound';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<EmployeesList />} />
+			<Route path="/add" element={<AddEmployee />} />
+			<Route path="/employees/edit/:id" element={<AddEmployee />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
 }
 
 export default App;
---
+++ src/components/EmployeesList.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import employeeService from '../services/employee.service';
+
+const EmployeesList = () => {
+	const [employees, setEmployees] = useState([]);
+
+	const init = () => {
+		employeeService
+			.getAll()
+			.then((response) => {
+				console.log('Printing employees data', response.data);
+				setEmployees(response.data);
+			})
+			.catch((error) => {
+				console.log('Something went wrong', error);
+			});
+	};
+
+	useEffect(() => {
+		init();
+	}, []);
+
+	const handleDelete = (id) => {
+		console.log('Printing id', id);
+		employeeService
+			.remove(id)
+			.then((response) => {
+				console.log('Employee deleted successfully', response.data);
+				init();
+			})
+			.catch((error) => {
+				console.log('Something went wrong', error);
+			});
+	};
+
+	return (
+		<div className="container">
+			<h3>List of Employees</h3>
+			<hr />
+			<div className="table-responsive">
+				<Link to="/add" className="btn btn-primary mb-2">
+					Add Employee
+				</Link>
+				<table className="table table-bordered table-striped">
+					<thead className="table-dark">
+						<tr>
+							<th>Name</th>
+							<th>Location</th>
+							<th>Department</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{employees.map((employee) => (
+							<tr key={employee.id}>
+								<td>{employee.name}</td>
+								<td>{employee.location}</td>
+								<td>{employee.department}</td>
+								<td>
+									<Link
+										className="btn btn-info"
+										to={`/employees/edit/${employee.id}`}
+									>
+										Update
+									</Link>
+									<button
+										className="btn btn-danger m-2"
+										onClick={() => {
+											handleDelete(employee.id);
+										}}
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+};
+
+export default EmployeesList;

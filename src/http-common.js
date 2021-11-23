ce7253f0ac0df0fa909af81ec90e59f61edadea7
Kevin-Estrada <ke81297@gmail.com>
2021-11-22 23:44:00 -0600

    Added one small mistake to the baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
 	// baseURL: 'http://localhost:8080/api/v1'
-	baseURL: 'https://spring-boot-react-frontend.herokuapp.com',
+	baseURL: 'https://spring-boot-react-frontend.herokuapp.com/api/v1',
 	headers: {
 		'Content-Type': 'application/json',
 	},
